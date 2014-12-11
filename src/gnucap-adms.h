b9dab2673d9d44dac4eed5d5e2281c406aa4c7da
Felix Salfelder <felix@salfelder.org>
2014-12-11 19:28:28 +0100

    remove extern from gnucap-adms.h

--- src/gnucap-adms.h
+++ src/gnucap-adms.h
@@ -1,21 +1,21 @@
 // these devices are provided by the simulator and need
 // not be implemented
 
-extern module R(p,n);
+module R(p,n);
    inout p,n;
 	electrical p,n;
 
 	parameter real r=1e3 from [0:inf);
 endmodule
 
-extern module C(p,n);
+module C(p,n);
    inout p,n;
 	electrical p,n;
 
 	parameter real c=1e-6 from [0:inf);
 endmodule
 
-extern module L(p,n);
+module L(p,n);
    inout p,n;
 	electrical p,n;
 
@@ -23,7 +23,7 @@ extern module L(p,n);
 endmodule
 
 // `ifdef USE_DEGR (FIXME, check gnucap capabilities)
-extern module rcd_exp(p, n, e);
+module rcd_exp(p, n, e);
 	inout p, n;
 	output e;
 	electrical p, n;
