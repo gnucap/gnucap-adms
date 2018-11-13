a47b4fb27fdb2f2a1c4a685ea2745e1f2e26e47a
Felix Salfelder <felix@salfelder.org>
2018-11-13 20:58:15 +0000

    this mirror (github) is dead, see
    http://git.savannah.gnu.org/cgit/gnucap/gnucap-adms.git
    or
    https://gitlab.com/gnucap/gnucap-adms

--- tests/include.2.sp
+++
@@ -1,8 +0,0 @@
-*some include test
-.load lang_adms.so
-
-.verilog
-
-`include "/doesnotexist"
-
-end
--- tests/includefile.v
+++
@@ -1,6 +0,0 @@
-
-options language=verilog
-
-module foo(1 2);
-definedinspice #(.dummy(0)) inst(1 2);
-endmodule
